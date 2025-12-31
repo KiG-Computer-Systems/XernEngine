6ef65484ee6b45be55128b8f82b2623113e302bb
KiG Organization <misteranthems5@gmail.com>
2025-12-31 23:49:45 +0500

    Enhance SpriteManager with error handling

--- src/components/spriteManager.js
+++ src/components/spriteManager.js
@@ -1,18 +1,77 @@
 // Менеджер спрайтов
 export class SpriteManager {
     constructor() {
+        if (!this || typeof this !== 'object') {
+            throw new TypeError('SpriteManager constructor called without "new" keyword');
+        }
+
         this.sprites = [];
+
+        try {
+            Object.seal(this);
+        } catch (error) {
+            if (error instanceof TypeError) {
+                throw new TypeError('SpriteManager instance cannot be modified');
+            } else {
+                throw error;
+            }
+        }
     }
 
     addSprite(sprite) {
-        this.sprites.push(sprite);
+        if (!sprite || typeof sprite !== 'object') {
+            throw new TypeError('sprite must be an object');
+        }
+
+        if (sprite instanceof Sprite) {
+            this.sprites.push(sprite);
+        } else {
+            throw new TypeError('sprite must be an instance of Sprite');
+        }
     }
 
     getSprite(index) {
-        return this.sprites[index];
+        if (typeof index !== 'number') {
+            throw new TypeError('index must be a number');
+        }
+
+        if (index < 0 || index >= this.sprites.length) {
+            throw new RangeError('index is out of range');
+        }
+
+        const sprite = this.sprites[index];
+        if (!sprite) {
+            throw new ReferenceError('sprite at index ' + index + ' is null or undefined');
+        }
+
+        return sprite;
     }
 
     drawAll(context) {
-        this.sprites.forEach(sprite => sprite.draw(context));
+        if (!context || typeof context !== 'object') {
+            throw new TypeError('context must be an object');
+        }
+
+        try {
+            this.sprites.forEach(sprite => {
+                if (!sprite || typeof sprite !== 'object') {
+                    throw new TypeError('sprite at index ' + this.sprites.indexOf(sprite) + ' is null or undefined');
+                }
+
+                if (!(sprite instanceof Sprite)) {
+                    throw new TypeError('sprite at index ' + this.sprites.indexOf(sprite) + ' is not an instance of Sprite');
+                }
+
+                sprite.draw(context);
+            });
+        } catch (error) {
+            if (error instanceof TypeError) {
+                console.error('TypeError: ' + error.message);
+            } else if (error instanceof ReferenceError) {
+                console.error('ReferenceError: ' + error.message);
+            } else {
+                throw error;
+            }
+        }
     }
 }
