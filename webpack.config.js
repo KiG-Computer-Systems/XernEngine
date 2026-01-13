c86de5b53e30935e3e9438c753a68bdf7317cfdb
KiG Organization <misteranthems5@gmail.com>
2026-01-13 21:40:29 +0500

    Enhance webpack config with plugins and loaders
    Updated webpack configuration to include HTML plugin, CSS and asset support, and improved dev server settings.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].[contenthash].js', 
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   devServer: {
-    static: './dist',
-    port: 8080
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    port: 9999,
+    open: true,
+    hot: true, 
+    compress: true 
   },
   module: {
     rules: [
@@ -16,10 +23,36 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env', 
+              '@babel/preset-react'
+            ]
+          }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i, 
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
       }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'] 
   },
   mode: 'development'
 };
